f877ee205eddebe8792789ce24058645264940d2
ola <alaalabdallua@gmail.com>
2024-11-11 09:56:48 +0200

    revers array two

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 var x = 0;
 function getQoutes() {
-  console.log(x);
-
   var Qoutes = [
     "“If you tell the truth, you don't have to remember anything.”",
     "“I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”",
@@ -10,12 +8,7 @@ function getQoutes() {
     "“In three words I can sum up everything I've learned about life: it goes on.”",
     "“Be the change that you wish to see in the world.”",
   ];
-  var Qoutes2 = [ "“Be the change that you wish to see in the world.”","“In three words I can sum up everything I've learned about life: it goes on.”",
-    "“Live as if you were to die tomorrow. Learn as if you were to live forever.”",
-      "“Always forgive your enemies; nothing annoys them so much.”",
-      "“If you tell the truth, you don't have to remember anything.”",
-      "“The best revenge is massive success.”"
-   ]
+  var Qoutes2 = Qoutes.reverse()
   var auther = [
     "― Mark Twain",
     "― Maya Angelou",
@@ -25,7 +18,7 @@ function getQoutes() {
     "― Mahatma Gandhi",
   ];
 
-  {
+  
     var number = Math.floor(Math.random() * Qoutes.length);
     if (x !== number) {
       document.getElementById("qoute").innerHTML = Qoutes[number];
@@ -34,7 +27,7 @@ function getQoutes() {
       document.getElementById("qoute").innerHTML = Qoutes2[number];
       document.getElementById("auther").innerHTML = `** ${auther[number]} **`;
     }
-  }
+  
 
   x = number;
 }
